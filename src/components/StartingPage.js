902544293130bcda35770adf32e66e183cae4a52
FenoJS <michalskiJS@gmail.com>
2019-04-03 01:51:43 +0200

    initial commit

---
+++ src/components/StartingPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const StartingPage = () => {
+  return (
+    <div>
+      <button className="start-btn">Start Game</button>
+    </div>
+  );
+};
+
+export default StartingPage;
